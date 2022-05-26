0470284fe6fca57125338c7219c045d49aafae59
Marc Doran <marc@Marcs-Air.lan>
2022-05-25 20:27:04 -0500

    heroku fix

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,19 +1,14 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react'
 import '../styles/Login.css'
 import vid from '../assets/bg_vid.mp4'
+
 const Login = () => {
-  const [data, setData] = useState('');
-
-
-
   return (
     <div className="login-page">
       <video autoPlay loop muted id="myVideo">
         <source src={vid} type="video/mp4" />
       </video>
-      <h1 className="welcome">Welcome to CS Social! {data.data}</h1>
+      <h1 className="welcome">Welcome to CS Social!</h1>
       <div className="login-form-container">
         <a href="https://codesmith-social.herokuapp.com/auth" className="githubOAuth">Sign In Using Github</a>
       </div>
--- server.js
+++ server.js
@@ -21,15 +21,15 @@ app.use(express.static(path.join(__dirname, './client/build')));
 app.use(cookieParser());
 
 // connect to atlas DB
-// mongoose.connect(
-//   process.env.MONGO_URI,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-// ).then(() => {
-//   console.log('Connected to DB');
-// });
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+).then(() => {
+  console.log('Connected to DB');
+});
 
 // root
 
